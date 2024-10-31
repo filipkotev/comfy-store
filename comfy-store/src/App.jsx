01242834acf232e943525e2e75e1829e55ca9076
Filip Kotev <filipkotev@yahoo.com>
2024-10-31 14:50:10 +0200

    Vite app setup

---
+++ comfy-store/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Orders } from './pages'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl'>Comfy store</h1>
+      <Orders />
+    </>
+  )
+}
+
+export default App
