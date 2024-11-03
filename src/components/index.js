95e58d18a9c921017da1ae975c974cfae322ee84
Filip Kotev <filipkotev@yahoo.com>
2024-11-03 18:31:08 +0200

    Header with nav and nav links created

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as FormInput } from './FormInput';
 export { default as SubmitBtn } from './SubmitBtn';
 export { default as ErrorElement } from './ErrorElement';
+export { default as Header } from './Header';
+export { default as Navbar } from './Navbar';
+export { default as NavLinks } from './NavLinks';
