e571dd1d9c93ba604fad9154827389353a7e058c
Filip Kotev <filipkotev@yahoo.com>
2024-11-03 19:29:45 +0200

    Hero component(with carousel) and About page created

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Header } from './Header';
 export { default as Navbar } from './Navbar';
 export { default as NavLinks } from './NavLinks';
 export { default as ThemeToggler } from './ThemeToggler';
+export { default as Hero } from './Hero';
