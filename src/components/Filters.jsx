fd701417807846279e336a88dd9bba36dd9ec5cb
Filip Kotev <filipkotev@yahoo.com>
2024-11-05 17:59:18 +0200

    Products page - filter params extracted from the url

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,8 @@ import { Form, useLoaderData, Link } from 'react-router-dom';
 import { FormInput, FormSelect, FormRange, FormCheckbox } from './';
 
 const Filters = () => {
-  const { meta } = useLoaderData();
+  const { meta, params } = useLoaderData();
+  const { search, company, category, shipping, order, price } = params;
 
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
@@ -11,46 +12,54 @@ const Filters = () => {
         type="search"
         label="search product"
         name="search"
+        defaultValue={search}
         size="input-sm"
       />
 
       {/* CATEGORIES */}
       <FormSelect
         label='select category'
-        name='category'
+        name="category"
+        defaultValue={category}
         list={meta.categories}
         size='select-sm'
       />
       {/* COMPANIES */}
       <FormSelect
         label='select company'
-        name='company'
+        name="company"
+        defaultValue={company}
         list={meta.companies}
         size='select-sm'
       />
       {/* ORDER */}
       <FormSelect
-        label='sort by'
-        name='order'
+        label="sort by"
+        name="order"
+        defaultValue={order}
         list={['a-z', 'z-a', 'high', 'low']}
-        size='select-sm'
+        size="select-sm"
       />
       {/* PRICE */}
       <FormRange
         label="select price"
         name="price"
+        price={price}
         size="range-sm"
       />
       {/* SHIPPING */}
       <FormCheckbox
         label="free shipping"
         name="shipping"
+        defaultValue={shipping}
         size="checkbox-sm"
       />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm capitalize">
         search
       </button>
+
+      {/* This way we reset the form because there is not params in the url - '/products', therefore all form fields will accept their default values */}
       <Link to="/products" className="btn btn-accent btn-sm">
         reset
       </Link>
